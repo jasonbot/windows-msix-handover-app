47baa341c368e009dc5a43f36cdcbc445552a62a
Jason Scheirer <jscheirer@makenotion.com>
2025-11-21 15:01:31 -0800

    What

--- cmd/checklisttest/main.go
+++ cmd/checklisttest/main.go
@@ -1,3 +1,62 @@
 package main
 
-func main() {}
+import (
+	"image"
+	"image/color"
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+)
+
+func run(window *app.Window) error {
+	theme := material.NewTheme()
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			var ops op.Ops
+			// This graphics context is used for managing the rendering state.
+			graphicContext := app.NewContext(&ops, e)
+
+			// Define an large label with an appropriate text:
+			title := material.H1(theme, "Hello, Gio")
+			subtitle := material.H2(theme, "Hello my baby")
+
+			// Change the color of the label.
+			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+			title.Color = maroon
+
+			// Change the position of the label.
+			title.Alignment = text.Middle
+			title.MaxLines = 1
+
+			// Draw the label to the graphics context.
+			dims := title.Layout(graphicContext)
+			{
+				log.Println("DDims", dims)
+				defer op.Offset(image.Point{0, dims.Size.Y}).Push(graphicContext.Ops).Pop()
+				subtitle.Layout(graphicContext)
+			}
+
+			// Pass the drawing operations to the GPU.
+			e.Frame(graphicContext.Ops)
+		}
+	}
+}
+
+func main() {
+	go func() {
+		window := new(app.Window)
+		err := run(window)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
--- go.mod
+++ go.mod
@@ -3,14 +3,25 @@ module github.com/jasonbot/windows-msix-handover-app
 go 1.25.3
 
 require (
+	gioui.org v0.9.0
 	github.com/goccy/go-yaml v1.18.0
+	github.com/shirou/gopsutil v3.21.11+incompatible
+	github.com/zzl/go-com v1.5.0
+	github.com/zzl/go-win32api v1.2.0
+	github.com/zzl/go-win32api/v2 v2.1.0
+	github.com/zzl/go-winrtapi v1.0.0
+	golang.org/x/sys v0.38.0
+)
+
+require (
+	gioui.org/shader v1.0.8 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/go-text/typesetting v0.3.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	github.com/tklauser/go-sysconf v0.3.16 // indirect
+	github.com/tklauser/numcpus v0.11.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	github.com/zzl/go-com v1.5.0 // indirect
-	github.com/zzl/go-win32api v1.2.0 // indirect
-	github.com/zzl/go-win32api/v2 v2.1.0 // indirect
-	github.com/zzl/go-winmd v1.0.0 // indirect
-	github.com/zzl/go-winrtapi v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+	golang.org/x/exp/shiny v0.0.0-20250408133849-7e4ce0ab07d0 // indirect
+	golang.org/x/image v0.26.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
