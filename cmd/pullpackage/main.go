c566661aeb86dad77a1c2b1eb6c42ff7e533bdf5
Jason Scheirer <jscheirer@makenotion.com>
2025-12-10 09:54:35 -0800

    Crash on uninit

--- cmd/pullpackage/main.go
+++ cmd/pullpackage/main.go
@@ -27,16 +27,43 @@ import (
 	"golang.org/x/sys/windows/registry"
 )
 
-type updateFile struct {
+type YamlUpdateFile struct {
 	Url    string `yaml:"url"`
 	Sha512 string `yaml:"sha512"`
 	Size   uint64 `yaml:"size"`
 }
 
-type updateStruct struct {
-	Files       []updateFile `yaml:"files"`
-	ReleaseDate string       `yaml:"releaseDate"`
-	Version     string       `yaml:"version"`
+type YamlUpdateStruct struct {
+	Files       []YamlUpdateFile `yaml:"files"`
+	ReleaseDate string           `yaml:"releaseDate"`
+	Version     string           `yaml:"version"`
+	Sha512      string           `yaml:"sha512"`
+	Path        string           `yaml:"path"`
+}
+
+type CPUArchitecture string
+
+const (
+	ArchArm64 CPUArchitecture = "arm64"
+	ArchAmd64 CPUArchitecture = "amd64"
+)
+
+type DesktopProduct struct {
+	ProductName  string
+	Architecture CPUArchitecture
+}
+
+var DesktopProductFeeds map[DesktopProduct]string
+
+func init() {
+	DesktopProductFeeds = map[DesktopProduct]string{
+		DesktopProduct{ProductName: "Notion", Architecture: ArchArm64}:     "https://desktop-release.notion-static.com/arm64-msix.yml",
+		DesktopProduct{ProductName: "Notion Dev", Architecture: ArchArm64}: "https://dev-desktop-release.s3.us-west-2.amazonaws.com/arm64-msix.yml",
+		DesktopProduct{ProductName: "Notion Stg", Architecture: ArchArm64}: "https://stg-desktop-release.s3.us-west-2.amazonaws.com/arm64-msix.yml",
+		DesktopProduct{ProductName: "Notion", Architecture: ArchAmd64}:     "https://desktop-release.notion-static.com/msix.yml",
+		DesktopProduct{ProductName: "Notion Dev", Architecture: ArchAmd64}: "https://dev-desktop-release.s3.us-west-2.amazonaws.com/msix.yml",
+		DesktopProduct{ProductName: "Notion Stg", Architecture: ArchAmd64}: "https://stg-desktop-release.s3.us-west-2.amazonaws.com/msix.yml",
+	}
 }
 
 func stopAppIfRunning(appName string) {
@@ -69,14 +96,13 @@ func stopAppIfRunning(appName string) {
 					for pr, _ := proc.IsRunning(); pr; pr, _ = proc.IsRunning() {
 						fmt.Println("Waiting to quit")
 					}
-					return
 				}
 			}
 		}
 	}
 }
 
-func uninstallAppIfInstalled(appName string) {
+func uninstallWin32AppIfInstalled(appName string) {
 	ui := `SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall`
 
 	for _, tk := range []registry.Key{registry.CURRENT_USER, registry.LOCAL_MACHINE} {
@@ -119,14 +145,38 @@ func findLatestMSIXUpdate(channelYamlURL string) (string, int64, string, error) 
 	}
 	defer resp.Body.Close()
 
-	var config updateFile
+	var config YamlUpdateStruct
 	if yamlSource, err := io.ReadAll(resp.Body); err == nil {
 		yaml.Unmarshal(yamlSource, &config)
 	} else {
 		return "", 0, "", err
 	}
 
-	return config.Url, int64(config.Size), config.Sha512, nil
+	log.Println("CXonfig", config)
+
+	for _, file := range config.Files {
+		if strings.HasSuffix(strings.ToLower(file.Url), ".msix") {
+			installerUrl := strings.Replace(file.Url, "-x64", "", 1)
+			if computedUrl, err := url.JoinPath(
+				channelYamlURL,
+				"..",
+				strings.Replace(
+					file.Url,
+					"-x64",
+					"",
+					1,
+				),
+			); err == nil {
+				installerUrl = computedUrl
+			}
+
+			log.Println("URL:", config.Path, "->", installerUrl)
+
+			return installerUrl, int64(file.Size), file.Sha512, nil
+		}
+	}
+
+	return "", 0, "", errors.New("no update msix found")
 }
 
 type writerWrapper struct {
@@ -241,28 +291,30 @@ func Co_AwaitWithProgress[R, P comparable](operation *winrt.IAsyncOperationWithP
 			if err := onComplete(operation, asyncStatus); err != nil {
 				returnVal = com.FAIL
 			}
-			//win32.PostThreadMessage(com.GetContext().TID, win32.WM_QUIT, 0, 0)
+			// win32.PostThreadMessage(com.GetContext().TID, win32.WM_QUIT, 0, 0)
 			done <- struct{}{}
 
 			return returnVal
 		},
 	)
 
-	//com.MessageLoop()
+	// com.MessageLoop()
 	<-done
 }
 
 func installMSIXFromDownloadsFolder(msixPath string) {
 	runtime.LockOSThread()
 	winrt.Initialize()
-	defer winrt.Uninitialize()
+	// defer winrt.Uninitialize()
 
-	msixURI := fmt.Sprint("file://", url.PathEscape(msixPath))
+	msixURI := fmt.Sprint("file://", strings.ReplaceAll(msixPath, "\\", "/"))
+	log.Println("MSIX", msixURI)
 	uri := winrt.NewUri_CreateUri(msixURI)
+	log.Println("MSIX URL:", msixURI)
 
 	pm := management.NewPackageManager()
 	if pm.IUnknown.GetIUnknown() == nil {
-		log.Println("Ooof")
+		log.Println("No package manager")
 		return
 	}
 
@@ -316,11 +368,11 @@ func Co_CreateInstanceByClassID[T comparable](clsid string, iid syscall.GUID) (*
 func runInstalledApp() {
 	runtime.LockOSThread()
 	winrt.Initialize()
-	defer winrt.Uninitialize()
+	// defer winrt.Uninitialize()
 
 	pm := management.NewPackageManager()
 	if pm.IUnknown.GetIUnknown() == nil {
-		log.Println("Ooof")
+		log.Println("No package manager")
 		return
 	}
 
@@ -331,11 +383,11 @@ func runInstalledApp() {
 			currentPackage := iterator.Get_Current()
 			if currentPackage != nil {
 				il := currentPackage.Get_Id()
-				log.Println("App", il.Get_Name())
-				if il.Get_Name() == "MPOSSIBL:E" {
-					msixURI := fmt.Sprint("shell:AppsFolder\\", url.PathEscape(il.Get_FullName()))
-					uri := winrt.NewUri_CreateUri(msixURI)
+				msixURI := fmt.Sprint("shell:AppsFolder\\", url.PathEscape(il.Get_FullName()))
+				uri := winrt.NewUri_CreateUri(msixURI)
+				log.Println("App", il.Get_Name(), msixURI)
 
+				if il.Get_Name() == "MPOSSIBL:E" {
 					ls, _ := Co_CreateInstanceByClassID[winrt.ILauncherStatics](
 						"Windows.System.Launcher",
 						winrt.IID_ILauncherStatics,
@@ -354,26 +406,21 @@ func runInstalledApp() {
 	}
 }
 
-type appConfig struct {
-	AppName        string
-	ChannelYamlURL string
-}
-
 func main() {
-	app := appConfig{
-		AppName:        "Notion Dev",
-		ChannelYamlURL: "https://fart.com/",
+	app := DesktopProduct{
+		ProductName:  "Notion Dev",
+		Architecture: CPUArchitecture(runtime.GOARCH),
 	}
 
-	stopAppIfRunning(app.AppName)
+	stopAppIfRunning(app.ProductName)
 
-	msixUrl, fileSize, sha512, err := findLatestMSIXUpdate(app.ChannelYamlURL)
-	if err != nil {
+	msixUrl, fileSize, sha512, err := findLatestMSIXUpdate(DesktopProductFeeds[app])
+	if err == nil {
 		msixPath := downloadMSIXToDownloadsFolder(msixUrl, fileSize, sha512)
 
 		if msixPath != "" {
-			uninstallAppIfInstalled(app.AppName)
 			installMSIXFromDownloadsFolder(msixPath)
+			uninstallWin32AppIfInstalled(app.ProductName)
 		}
 	}
 }
